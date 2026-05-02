616f440da325ee11d62d18023bdfcb018240a9ad
Isabel <isabelmarpolo9@gmail.com>
2026-05-02 16:36:24 +0200

    Actualizar Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,7 +5,10 @@ export default function Header() {
     <header className="bg-purple-700 shadow-md">
       <div className="max-w-5xl mx-auto px-4 py-3 flex items-center">
         <Link to="/">
-          <img src="/logo_con_fondo.jpg" alt="Bigotes Mudejaresv" className="h-12 w-auto" title="Gestión de Colonias Felinas" />
+          <img src="/logo_con_fondo.jpg" alt="Bigotes Mudejaresv" className="h-12 w-auto" />
+          <h1 style={{ color: 'white', margin: 0 }}>
+          Gestión de Colonias Felinas
+          </h1>
         </Link>
       </div>
     </header>
