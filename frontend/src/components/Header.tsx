492a4fdfbc55e9035ed31838ac7a4ddd11ea3f6d
Isabel <isabelmarpolo9@gmail.com>
2026-05-02 16:22:50 +0200

    actualizacioón del header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,7 +5,8 @@ export default function Header() {
     <header className="bg-purple-700 shadow-md">
       <div className="max-w-5xl mx-auto px-4 py-3 flex items-center">
         <Link to="/">
-          <img src="/logo.png" alt="Bigotes Mudejaresv" className="h-12 w-auto" />
+          <img src="/logo_con_fondo.jpg" alt="Bigotes Mudejaresv" className="h-12 w-auto" />
+          Gestión de Colonias Felinas
         </Link>
       </div>
     </header>
